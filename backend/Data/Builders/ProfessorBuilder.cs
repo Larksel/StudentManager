e5f7ed02e65c5e7cc1248f5b3c3a8727b1e8ea8b
Larksel <lemuelgames@outlook.com>
2025-03-07 00:50:44 -0300

    :sparkles: Builders de Professor e Student criados

---
+++ backend/Data/Builders/ProfessorBuilder.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentManager.WebAPI.Objects.Models;
+
+namespace StudentManager.WebAPI.Data.Builders;
+
+public class ProfessorBuilder
+{
+    public static void Build(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Professor>().HasKey(p => p.Id);
+        modelBuilder.Entity<Professor>().Property(p => p.Name).IsRequired().HasMaxLength(100);
+        modelBuilder.Entity<Professor>().Property(p => p.Email).IsRequired().HasMaxLength(100);
+        modelBuilder.Entity<Professor>().Property(p => p.PasswordHash).IsRequired().HasMaxLength(256);
+
+        modelBuilder.Entity<Professor>()
+            .HasData(new List<Professor>
+            {
+                new (1, "José Carlos", "josecarlos@gmail.com", "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92")
+            });
+    }
+}
---
+++ backend/Data/Builders/StudentBuilder.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentManager.WebAPI.Objects.Models;
+
+namespace StudentManager.WebAPI.Data.Builders;
+
+public class StudentBuilder
+{
+    public static void Build(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>().HasKey(p => p.Id);
+        modelBuilder.Entity<Student>().Property(p => p.Name).IsRequired().HasMaxLength(100);
+
+        modelBuilder.Entity<Student>()
+            .HasData(new List<Student>
+            {
+                new (1, "Miguel Silva"),
+                new (2, "Gabriel Oliveira")
+            });
+    }
+}
