52ee64f369806f5aa8b3c7120bf56570d9d1ae29
Larksel <lemuelgames@outlook.com>
2025-03-07 00:26:52 -0300

    :sparkles: feat: Models criadas

---
+++ backend/Objects/Models/Professor.cs
@@ -0,0 +1,26 @@
+﻿namespace StudentManager.WebAPI.Objects.Models;
+
+public class Professor
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+
+    // Relacionamento muitos-para-muitos com Alunos
+    public ICollection<Student> Students { get; set; }
+
+    public Professor()
+    {
+        Students = [];
+    }
+
+    public Professor(int professorId, string name, string email, string passwordHash)
+    {
+        Id = professorId;
+        Name = name;
+        Email = email;
+        PasswordHash = passwordHash;
+        Students = [];
+    }
+}
---
+++ backend/Objects/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿namespace StudentManager.WebAPI.Objects.Models;
+
+public class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    // Relacionamento muitos-para-muitos com Professores
+    public ICollection<Professor> Professors { get; set; }
+
+    public Student()
+    {
+        Professors = [];
+    }
+
+    public Student(int studentId, string name)
+    {
+        Id = studentId;
+        Name = name;
+        Professors = [];
+    }
+}
