65547ab7afa5f62f2ab6a1214959b484df836dc6
Larksel <lemuelgames@outlook.com>
2025-03-07 01:21:49 -0300

    :sparkles: feat: AppDbContext criado e configurado

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using StudentManager.WebAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
